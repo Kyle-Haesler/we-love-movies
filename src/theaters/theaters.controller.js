2c9126fb82e1aa560d4ac9bfa502c6aba1a9537c
Kyle Haesler <kyle.haesler@gmail.com>
2023-08-23 09:18:17 -0400

    Theatre route working

--- src/theaters/theaters.controller.js
+++ src/theaters/theaters.controller.js
@@ -5,6 +5,7 @@ const asyncErrorBoundary = require("../errors/asyncErrorBoundary")
 async function list(req, res, next){
     const data = await service.list()
     res.json({data})
+    console.log(data[0])
 }
 
 
